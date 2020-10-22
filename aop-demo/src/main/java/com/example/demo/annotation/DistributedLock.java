fd90830dd5502fdcb99bea18a64af1e3a227fcd6
xuyikai1 <xuyikai123@qq.com>
2020-10-22 20:43:42 +0800

    feat: 【aop-demo】1.Aop切面与Java自定义注解的应用（a.日志切面 b.分布式锁切面）

---
+++ aop-demo/src/main/java/com/example/demo/annotation/DistributedLock.java
@@ -0,0 +1,28 @@
+package com.example.demo.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * @Author: Xuyk
+ * @Description: 分布式锁
+ * @Date: 2020/9/24
+ */
+// 运行时使用该注解
+@Retention(RetentionPolicy.RUNTIME)
+// 作用于方法
+@Target(ElementType.METHOD)
+@Documented
+public @interface DistributedLock {
+
+    /**
+     * 尝试加锁的等待时间 默认2000毫秒
+     * @return
+     */
+    long waitTime() default 2000L;
+    /**
+     * 多少时间后自动释放锁 默认3000毫秒
+     * @return
+     */
+    long leaseTime() default 3000L;
+
+}
