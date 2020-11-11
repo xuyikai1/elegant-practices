206c53f28a5013587ad916f4baf2f3e61b0ccf8f
xuyikai1 <xuyikai123@qq.com>
2020-11-11 15:59:11 +0800

    feat: 【oss-demo】1.新增oss上传图片demo 2.授权访问

---
+++ oss-demo/src/main/java/com/example/ossdemo/OssDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ossdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OssDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OssDemoApplication.class, args);
+    }
+
+}
---
+++ oss-demo/src/test/java/com/example/ossdemo/OssDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ossdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OssDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
