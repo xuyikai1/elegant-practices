7715363000afc3540bdab88d5fe333c6606cd8e3
xuyikai1 <xuyikai123@qq.com>
2020-11-12 17:30:35 +0800

    feat: 【logback-demo】1.logback打印info/error级别日志到指定目录 2.控制台可输出sql语句便于调试

---
+++ logback-demo/src/main/java/com/example/logbackdemo/mapper/StudentMapper.java
@@ -0,0 +1,8 @@
+package com.example.logbackdemo.mapper;
+
+
+import com.example.logbackdemo.MyMapper;
+import com.example.logbackdemo.pojo.Student;
+
+public interface StudentMapper extends MyMapper<Student> {
+}
