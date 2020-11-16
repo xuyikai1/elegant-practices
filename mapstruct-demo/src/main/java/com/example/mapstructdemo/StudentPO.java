432f8b281a7e4309b6f5eb5f9cd132cda547c35c
xuyikai1 <xuyikai123@qq.com>
2020-11-16 14:28:22 +0800

    feat: 【mapstruct-demo】1.对象转换框架试水demo记录

---
+++ mapstruct-demo/src/main/java/com/example/mapstructdemo/StudentPO.java
@@ -0,0 +1,25 @@
+package com.example.mapstructdemo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author: Xuyk
+ * @Description:
+ * @Date: 2020/11/16
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentPO {
+
+    private String id;
+
+    private String name;
+
+    private Integer age;
+
+}
